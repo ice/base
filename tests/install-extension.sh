deb2507917330412e44a504c60c52e6474df8578
mruz <mruz@poczta.onet.pl>
2018-03-25 23:07:07 +0100

    CI, install extension

--- tests/install-extension.sh
+++ tests/install-extension.sh
@@ -2,5 +2,15 @@
 
 git clone -q --depth=1 https://github.com/ice/framework.git
 cd framework/
-. ./install
+
+PHP7=`php -r "echo (int) version_compare(PHP_VERSION, '7.0.0', '>=');"`
+
+if (( "$PHP7" -eq 1 )); then
+    cd build/php7
+else
+    cd build/php5
+fi
+
+source ./install
+
 phpenv config-add ../../tests/ci/ice.ini
