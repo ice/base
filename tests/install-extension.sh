0068aa6060798714499e8e4330ad3a4a7928feee
mruz <mruz@poczta.onet.pl>
2018-03-25 23:32:33 +0100

    Travis, mongodb fix

--- tests/install-extension.sh
+++ tests/install-extension.sh
@@ -2,15 +2,5 @@
 
 git clone -q --depth=1 https://github.com/ice/framework.git
 cd framework/
-
-PHP7=`php -r "echo (int) version_compare(PHP_VERSION, '7.0.0', '>=');"`
-
-if (( $PHP7 == 1 )); then
-    cd build/php7
-else
-    cd build/php5
-fi
-
-source ./install
-
+. ./install
 phpenv config-add ../../tests/ci/ice.ini
